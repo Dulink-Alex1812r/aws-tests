7b9e056cb6e228a4f60d94b876255926bf582591
alex1812r <alex1812r@gmail.com>
2020-10-29 14:06:24 -0400

    aws route

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ app.get('/', (req, res) => {
   res.send('SERVER WORKING!');
 });
 
+app.get('/aws', (req, res) => {
+  res.send('AWS IS AWESOME!')
+});
+
 app.listen(process.env.PORT || 8000, () => {
   console.log("SERVER WORKING!")
 });
